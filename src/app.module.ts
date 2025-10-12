16a3e9f667d9c6b13f65747bc90f9bcc037ef8c0
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-12 09:30:17 -0300

    feat: atualizando app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { BotModule } from './bot/bot.module';
+import { PixModule } from './pix/pix.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true}),
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.get<string>('TELEGRAM_BOT_TOKEN'),
+      }),
+    }),
+    BotModule,
+    PixModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
