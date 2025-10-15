da01eb0ac720c9d7d01a71c5169dc4ee91f2f359
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-15 11:02:25 -0300

    configuração do webhook

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const bot = app.get(getBotToken());
+  app.use(bot.webhookCallback('/telegraf'))
   await app.listen(3000);
 }
 bootstrap();
