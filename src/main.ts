a138a70989df3c29fa27c645204b3543d9501b43
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-15 11:46:31 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-
   const bot = app.get(getBotToken());
-  app.use(bot.webhookCallback('/telegraf'))
 
+  const webhookPath = `/telegraf`;
 
-  
+  app.use(bot.webhookCallback(webhookPath));
+  if (process.env.NODE_ENV === 'production') {
+    const domain = process.env.BOT_DOMAIN;
+    if (!domain) {
+      throw new Error('A variável de ambiente BOT_DOMAIN não está definida!');
+    }
+    const webhookUrl = `https://${domain}${webhookPath}`;
+    await bot.telegram.setWebhook(webhookUrl);
+    console.log(`Webhook configurado para: ${webhookUrl}`);
+  }
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
