6d645fdddc1ab80bb7f367e783a2a34b3618a0aa
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-15 11:31:55 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,8 @@ async function bootstrap() {
 
 
   const bot = app.get(getBotToken());
-  const webhookPath = '/telegraf';
+  app.use(bot.webhookCallback('/telegraf'))
 
-
-  app.use(bot.webhookCallback(webhookPath));
-
-  
-  if (process.env.NODE_ENV === 'production') {
-    const domain = process.env.BOT_DOMAIN;
-    if (!domain) {
-      throw new Error('A variável de ambiente BOT_DOMAIN não está definida!');
-    }
-    const webhookUrl = `https://${domain}${webhookPath}`;
-    await bot.telegram.setWebhook(webhookUrl);
-    console.log(`Webhook configurado para: ${webhookUrl}`);
-  }
 
   
   await app.listen(process.env.PORT || 3000);
