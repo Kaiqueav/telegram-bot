c7268d0e2d0e2eadf940a17b7063b8476e4c77fa
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-14 21:06:34 -0300

    config modules

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BotUpdate } from './bot.update';
+import { ConfigModule } from '@nestjs/config';
+import { MercadoPagoModule } from 'src/mercado-pago/mercado-pago.module';
+
 
 @Module({
+    imports:[MercadoPagoModule, ConfigModule],
 providers:[BotUpdate]
 })
 export class BotModule {}
