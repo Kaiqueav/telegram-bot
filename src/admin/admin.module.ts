29cf0850a61b09be5150ee16173a931a1f36e3e4
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-15 14:24:04 -0300

    correação de delete

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { UserModule } from 'src/user/user.module';
 
