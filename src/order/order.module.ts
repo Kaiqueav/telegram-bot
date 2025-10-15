02a934174f1dba373a3012eaa17ba187a189babd
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-14 23:15:29 -0300

    alterando modulo

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,7 +2,8 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Order } from 'mercadopago';
+import { Order } from './entities/order.entity';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order])],
