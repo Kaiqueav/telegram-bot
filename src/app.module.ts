05efaecc0c6cffa83dd22654b63c0a0e426d8c9b
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-14 21:35:07 -0300

    tirando modulo desnecessario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,46 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TelegrafModule } from 'nestjs-telegraf';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { BotModule } from './bot/bot.module';
 import { MercadoPagoModule } from './mercado-pago/mercado-pago.module';
 import { OrderModule } from './order/order.module';
 import { NotificationModule } from './notification/notification.module';
+import { UserModule } from './user/user.module';
+
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
+    
     TelegrafModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         token: configService.get<string>('TELEGRAM_BOT_TOKEN'),
       }),
-        inject: [ConfigService], 
+      inject: [ConfigService],
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    
     BotModule,
     MercadoPagoModule,
     OrderModule,
     NotificationModule,
+    UserModule,
+
   ],
   controllers: [AppController],
   providers: [AppService],
