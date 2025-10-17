92d316cd157b61d0ae7fa4109f7635a45d1df25d
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-17 12:49:31 -0300

    configurando para lidar com mutiplos webhooks

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.use('/mercadopago/webhook', express.raw({ type: '*/*' }));
-
+  const bot = app.get(getBotToken());
+  app.use(bot.webhookCallback('/telegraf'));
 
   await app.listen(process.env.PORT || 3000);
 }
